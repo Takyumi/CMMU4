5d693e4ae7e8eaeab3ac416b89d2f5f9e66354d0
Takumi S Natsume <145226446+Takyumi@users.noreply.github.com>
2024-12-18 16:57:51 -0500

    Update overviewerConfig.js

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -43,7 +43,7 @@ var overviewerConfig = {
         {
             "name": "Overviewer Render (lighting)",
             "zoomLevels": 8,
-            "defaultZoom": 1,
+            "defaultZoom": 4,
             "maxZoom": 8,
             "path": "world-lighting",
             "base": "",
